e62796a641eb9987346d836b9ee3f4deae6c86e7
ananthvignesh0 <ananathvignesh@gmail.com>
2026-02-25 09:43:06 +0530

    UC1: Add welcome message module

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,2 +1,15 @@
 public class PalindromeCheckerApp {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Palindrome Checker App " );
+        System.out.println("Version 1.0");
+        System.out.println("This application checks whether a given string is a palindrome.");
+        System.out.println("Program started successfully.");
+        System.out.println("Proceeding to Palindrome Validation...");
+    }
+
+
+
 }
