013bca6b6c77b579e4f317b7b796a14008e1ac0d
krishnapandey06 <krishpandey26@gmail.com>
2026-03-12 08:43:15 +0530

    UC3:Hardcoded palindrome

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,2 +1,26 @@
-public class PalindromeCheckerApp {
+import java.util.Scanner;
+
+public class palindromecheckerapp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+        boolean isPalindrome = input.equals(reversed);
+
+        System.out.println("Original text : " + input);
+        System.out.println("Reversed text : " + reversed);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        scanner.close();
+    }
 }
