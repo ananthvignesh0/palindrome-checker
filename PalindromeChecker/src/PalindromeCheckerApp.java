0c9823c08c68d040feae84abe15ae884aece55f3
ananthvignesh0 <ananthvignesh0@gmail.com>
2026-03-12 09:07:00 +0530

    UC11:Hardcoded palindrome

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,27 +1,41 @@
+mport java.util.Stack;
+
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String input) {
+        if (input == null) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        int length = input.length();
+
+        for (int i = 0; i < length; i++) {
+            stack.push(input.charAt(i));
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (input.charAt(i) != stack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.println("Welcome to the Object-Oriented Palindrome Checker");
+        System.out.print("Enter a Name: ");
         String name = scanner.nextLine();
 
-        // UC10: Normalize string by using regex to remove spaces and converting to lowercase
-        String normalized = name.replaceAll("\\s+", "").toLowerCase();
+        PalindromeChecker checker = new PalindromeChecker();
+        boolean isPalindrome = checker.checkPalindrome(name);
 
-        int n = normalized.length();
-        boolean isPalindrome = check(normalized, 0, n - 1);
         System.out.println("Input : " + name + "\nIs Palindrome? : " + isPalindrome);
         scanner.close();
-    }
-
-    private static boolean check(String s, int start, int end) {
-        if (start >= end) {
-            return true;
-        }
-
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-
-        return check(s, start + 1, end - 1);
     }
 }
